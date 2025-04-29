30fb3e3fd5be512a44d944e35b41669ff67c2eab
Logan C. <34218953+Coit14@users.noreply.github.com>
2025-04-28 22:00:44 -0500

    restored

--- client/src/components/Pages/Menu.jsx
+++ client/src/components/Pages/Menu.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Menu.css';
-import schwabsLogo from '../../assets/schwabs-logo.png';
 
 const Menu = () => {
     return (
@@ -14,9 +13,6 @@ const Menu = () => {
                 <div className="menu-item-header">
                     <h2>Hot Dogs</h2>
                     <span className="price">$4.00</span>
-                    <div className="schwabs-logo-wrapper">
-                        <img src={schwabsLogo} alt="Schwab's Finest Logo" className="schwabs-logo" />
-                    </div>
                 </div>
                 <ul className="menu-items">
                     <li>No. 1 - Mustard, Chili & Onions</li>
