bd6bba317a6a4d2cb1e08f3cd5826e23fbc82b4b
Logan C. <34218953+Coit14@users.noreply.github.com>
2025-06-17 15:29:55 -0500

    Update printfulService.js

--- client/src/services/printfulService.js
+++ client/src/services/printfulService.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { API_URL } from '../config/config.js';
 
+const BASE_URL = `${API_URL}/api`;
+
 export const getPublishedProducts = async () => {
-  const res = await axios.get('/api/products');
+  const res = await axios.get(`${BASE_URL}/products`);
   return res.data;
 };
 
 export const getProduct = async (id) => {
-  const res = await axios.get(`/api/printful/products/${id}`);
+  const res = await axios.get(`${BASE_URL}/printful/products/${id}`);
   return res.data;
 }; 
